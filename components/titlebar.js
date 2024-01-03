dddd981625b1e5501bce190def5aecf244be1d92
Michael Goh <github@jwgoh.com>
2024-01-03 09:50:00 -0600

    Update titlebar.js

--- components/titlebar.js
+++ components/titlebar.js
@@ -1,6 +1,6 @@
 export default function TitleBar({ children }) {
   return (
-    <div className="flex w-full h-20 items-center p-5 px-10 border-white-100 border text-white-800">
+    <div className="flex w-full h-20 items-center p-5 px-10 border-white-100 border text-white-100">
       <h1>{children}</h1>
     </div>
   );
