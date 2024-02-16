d1c274299cc58ac0b49386a30f689de70c9d752e
Michael Goh <github@jwgoh.com>
2024-02-16 12:29:27 -0600

    bug fixes

--- components/staffing_item.js
+++ components/staffing_item.js
@@ -65,6 +65,7 @@ export default function StaffingItem({ event_id, email_id, field_name }) {
                 setUpdated((prevUpdated) => prevUpdated + 1);
               } else {
                 console.log("removing");
+                console.log(prevStaffing);
                 setStaffed((prevStaffing) =>
                   prevStaffing.filter((staff) => staff !== email_id)
                 );
